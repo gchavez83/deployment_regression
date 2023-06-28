da863db9d3a81ad317d0aa0f8ef283f2c15b0816
gchavez83 <61848325+gchavez83@users.noreply.github.com>
2023-06-28 15:32:10 -0600

    Update app_1.py

--- app_1.py
+++ app_1.py
@@ -29,4 +29,4 @@ input_df = pd.DataFrame([input_dict])
 if predict_button:
     out = predict(model, input_df)
     out = '$' + '{0:,.2f}'.format(out)
-    st.success(f'La predicción del precio es {out}')
+    st.warning(f'La predicción del precio es {out}')
